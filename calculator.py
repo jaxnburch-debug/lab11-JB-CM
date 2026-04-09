521309984bf35c270bea4f4f9864cf5e778b8518
jaxnburch-debug <jaxn.burch@gmail.com>
2026-04-09 13:11:32 -0400

    initial add calc

--- calculator.py
+++ calculator.py
@@ -1,11 +1,22 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a + b
 
+def subtract(a, b):
+    return a - b
 
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        raise ZeroDivisionError("Cant divide by zero")
+
+def logarithm(a, b):
+    return math.log(a, b)
+
+def exponent(a, b):
+    return a ** b
