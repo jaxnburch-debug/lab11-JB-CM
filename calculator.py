46e4a66031f6c499f1601c607995ec4988b769f4
jaxnburch-debug <jaxn.burch@gmail.com>
2026-04-12 21:34:19 -0400

    starting over: modified calculator p1 0.2

--- calculator.py
+++ calculator.py
@@ -1,33 +1,22 @@
 import math
 
-
-def square_root(a):
-    if a < 0:
-        raise ValueError("Cant calculate square root of a negative number")
-    return math.sqrt(a)
-
-def hypotenuse(a, b):
-    return math.hypot(a, b)
-
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
-def div(a, b):
+def divide(a, b):
     try:
         return a / b
     except ZeroDivisionError:
         raise ZeroDivisionError("Cant divide by zero")
 
 def logarithm(a, b):
-    if a <= 0 or b <= 0:
-        raise ValueError("Logarithm args must be positive")
     return math.log(a, b)
 
-def exp(a, b):
+def exponent(a, b):
     return a ** b
